3fb10564600ab910a06b3877979f280e5dcc918f
unknown <reginegabuyo@gmail.com>
2024-07-18 03:25:23 +0200

    feat: aded changes to discoverMovies.jsx

--- src/Components/DiscoverMovies.jsx
+++ src/Components/DiscoverMovies.jsx
@@ -5,36 +5,32 @@ import { SimpleGrid } from '@chakra-ui/react';
 function DiscoverMovies({ movies, searchMovie }) {
 
   const filteredMovies = movies.filter(movie => {
-    const title = movie.title ? movie.title.toLowerCase() : '';
-    const name = movie.name ? movie.name.toLowerCase() : '';
+    const title = movie.movieId.title;
 
-    return (
-      title.includes(searchMovie.toLowerCase()) ||
-      name.includes(searchMovie.toLowerCase())
-    );
+
+    return title.includes(searchMovie.toLowerCase());
   });
 
+
   return (
-    <>
-      <section>
-        <SimpleGrid
-          columns={{ base: 2, md: 4, lg: 5, xl: 6 }}
-          spacing={[6, 6, 6, 10]}
-          size='lg'
-          mt='10'
-          padding={{ lg: 7, xl: 10 }}
-        >
-          {filteredMovies.map((movie) => (
-            <MovieCard
-              key={movie.id}
-              id={movie.id}
-              title={movie.title || movie.name || 'Untitled'}
-              posterPath={movie.poster_path}
-            />
-          ))}
-        </SimpleGrid>
-      </section>
-    </>
+    <section>
+      <SimpleGrid
+        columns={{ base: 2, md: 4, lg: 5, xl: 6 }}
+        spacing={[6, 6, 6, 10]}
+        size='lg'
+        mt='10'
+        padding={{ lg: 7, xl: 10 }}
+      >
+        {filteredMovies.map((movie, index) => (
+          <MovieCard
+            key={index}
+            id={movie.id}
+            title={movie.movieId.title}
+            posterPath={movie.movieId.poster_path}
+          />
+        ))}
+      </SimpleGrid>
+    </section>
   );
 }
 
